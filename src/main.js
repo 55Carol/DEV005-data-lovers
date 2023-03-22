85c7f67ade23614534370ccbe7d5d78c6695334c
carol robles <carolrobleseduc@gmail.com>
2023-03-22 17:35:28 -0300

    guardar cambio filtrado n

--- src/main.js
+++ src/main.js
@@ -5,15 +5,65 @@ console.log(example, data);
 const datosghibli = data.films;
 console.log(datosghibli)
 
-//Elementos del DOM
-const search = document.querySelector('#search')
 
+//Elementos del DOM para cuadro de busqueda por nombre de pelicula
+const search = document.querySelector('#search')
 //Evento
 search.addEventListener('keyup', () => {
     console.log(search.value);
-}) 
-for (let i = 0; i <= datosghibli.length; i++) {
-    const nombrePeliculas = datosghibli[i].title
-    console.log(nombrePeliculas);
+    filtradoPornombre(); //parametrizar  la funcion  y asi darle un argumento
 }
+) 
+// for para iterar por cada nombre de cada pelicula
+for (let i = 0; i <= datosghibli.length; i++) {
+const nombrePeliculas = datosghibli.title
+ 
+}
+
+//Metodo filter -filtrado por nombre de pelicula 
+datosghibli.filter(film => film.title.indexOf(search.value)>-1)
+function filtradoPornombre() //parametro
+{const resultado = datosghibli.filter(film => film.title.indexOf(search.value) > -1);
+console.log(resultado);
+} 
+
+
+//Elemento del dom en botones de busqueda
+ 
+//leer y guardar valor de caja de busqueda por puntaje
+const puntaje = document.getElementById("puntaje").value;
+console.log(puntaje);
+
+//leer y guardar el valor de caja de busqueda por año
+const año = document.getElementById("año").value;
+console.log(año);
+//metodo filter para puntaje
+
+
+
+//metodo filter para año
+
+//  forEach para Iterar -- solo sirve para array (estudiar a detalle)
+//const ponster = []
+let posters = ''
+const contenedor = document.getElementById('contenedor')
+datosghibli.forEach((datoObj) => {
+    posters += 
+    `
+    <div class= "box" >
+        <div class="tarjeta">${datoObj.title}
+        </div>
+        <div> <img class="posterPe" src="${datoObj.poster}">
+        </div>
+    </div> 
+    `
+    console.log(posters)
+})
+contenedor.innerHTML = posters
+
+ 
+
+
+
+
 
