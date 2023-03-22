50c88300d2152ca22ce8f2f2ce0ba2ad95d0ca90
Kyoko931 <87785131+Kyoko931@users.noreply.github.com>
2023-03-22 12:37:49 -0600

    filtrado por nombre

--- src/data.js
+++ src/data.js
@@ -1,9 +1,21 @@
-// estas funciones son de ejemplo
+// Función para imprimir datos en pantalla
+export const titlesFilms = names => names.map(name => `
+<div class="card">
+  <div class="title"><h2>${name.title}</h2></div>
+  <div class="img">
+    <img src="${name.poster}" alt="movies">
+  </div>
+  <div class="text">
+    <div class="score"><h3>Score: ${name.rt_score}</h3></div>
+    <div class="year"><h4>Year: ${name.release_date}</h4></div>
+  </div>
+</div>`).join('')
 
-export const example = () => {
-  return 'example';
-};
+/*export const yearsFilms = years => years.map(year => `
+<option> ${year.release_date} </option>
+`).join('')
+
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
